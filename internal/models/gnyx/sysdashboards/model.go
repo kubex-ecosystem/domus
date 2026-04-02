1748ea61d65bf85e0fb649968d774ef0c1a8fdc9
Rafael Mori <faelmori@gmail.com>
2026-04-01 23:35:39 -0300

    refactor: reformat struct tag alignment and reorder imports in model definitions

--- internal/models/gnyx/sysdashboards/model.go
+++ internal/models/gnyx/sysdashboards/model.go
@@ -5,15 +5,15 @@ import (
 )
 
 type SysDashboards struct {
-	ID          *string `json:"id,omitempty" gorm:"column:id;primaryKey"`
-	TenantID    *string `json:"tenant_id,omitempty" gorm:"column:tenant_id"`
+	ID       *string `json:"id,omitempty" gorm:"column:id;primaryKey"`
+	TenantID *string `json:"tenant_id,omitempty" gorm:"column:tenant_id"`
 
 	Title       string  `json:"title" gorm:"column:title"`
 	Description *string `json:"description,omitempty" gorm:"column:description"`
 
-	CreatedAt   time.Time `json:"created_at" gorm:"column:created_at"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"column:updated_at"`
-	Active      bool      `json:"active" gorm:"column:active"`
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at"`
+	Active    bool      `json:"active" gorm:"column:active"`
 }
 
 func (SysDashboards) TableName() string {
--- internal/models/gnyx/syswidgets/model.go
+++ internal/models/gnyx/syswidgets/model.go
@@ -8,13 +8,13 @@ type SysWidgets struct {
 	ID          *string `json:"id,omitempty" gorm:"column:id;primaryKey"`
 	DashboardID *string `json:"dashboard_id,omitempty" gorm:"column:dashboard_id"`
 
-	Type        string  `json:"type" gorm:"column:type"`
-	Size        string  `json:"size" gorm:"column:size;type:jsonb"` // Store as JSONB string representation in DB e.g. {"cols":12,"rows":4}
-	Config      string  `json:"config" gorm:"column:config;type:jsonb"` // Store as JSONB string representation in DB, e.g. {"title": "xyz", ...}
+	Type   string `json:"type" gorm:"column:type"`
+	Size   string `json:"size" gorm:"column:size;type:jsonb"`     // Store as JSONB string representation in DB e.g. {"cols":12,"rows":4}
+	Config string `json:"config" gorm:"column:config;type:jsonb"` // Store as JSONB string representation in DB, e.g. {"title": "xyz", ...}
 
-	CreatedAt   time.Time `json:"created_at" gorm:"column:created_at"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"column:updated_at"`
-	Active      bool      `json:"active" gorm:"column:active"`
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at"`
+	Active    bool      `json:"active" gorm:"column:active"`
 }
 
 func (SysWidgets) TableName() string {
